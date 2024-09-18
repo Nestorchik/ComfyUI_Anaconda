b69ea2954d54b298f40e7b3349a300423836cf8c
NStor <andyr@mail.ru>
2024-09-19 02:30:36 +0300

    Create install_git-comfyui_conda.bat

---
+++ install_git-comfyui_conda.bat
@@ -0,0 +1,3 @@
+git clone https://github.com/comfyanonymous/ComfyUI.git d:\0\ComfyUI
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git d:\0\ComfyUI\custom_nodes\ComfyUI-Manager
+conda create -n venv_comfy312 python=3.12 -y && activate venv_comfy312 && conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia -y && conda install einops transformers safetensors tokenizers sentencepiece pyyaml scipy psutil kornia aiohttp -y && pip install torchsde spandrel soundfile && conda env export > d:\0\comfyui_required.yml && python d:\0\ComfyUI\main.py --windows-standalone-build
